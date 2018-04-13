6dd0b32d1619131c0a174b435e92dbbade9aa588
ouyang <allanouyang@163.com>
2018-04-13 14:55:56 +0800

    add compute.sh

--- shell-practice/parameter.sh
+++ shell-practice/parameter.sh
@@ -6,3 +6,11 @@ echo "第一个参数：$1";
 echo "第二个参数：$2";
 echo "第三个参数：$3";
 echo "\$#$# \$*$* \$\$$$ \$!$! \$@$@ \$-$- \$?$?"
+
+for i in "$*"; do
+    echo $i
+done
+
+for i in "$@"; do
+    echo $i
+done
