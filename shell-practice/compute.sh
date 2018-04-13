45e5ce1dfb82ad2701e9697345de691236835c8a
ouyang <allanouyang@163.com>
2018-04-13 15:11:20 +0800

    modify compute.sh

--- shell-practice/compute.sh
+++ shell-practice/compute.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 val=`expr 2 + 2`
 echo $val
+
+a=10;
+b=20;
+var_add=`expr $a + $b`;
+echo "a + b = $var_a";
+var_reduce=`expr $a - $b`;
+echo "a - b = $var_reduce";
+var_multi=`expr $a \* $b`;
+echo "a * b = $var_multi";
+var_div=`expr $b / $a`;
+echo "b / a = $var_div";
+var_mod=`expr $b % $a`;
+echo "b % a = $var_mod";
+var_bool = [$a != $b];
+if $var_bool then
+    echo "a不等于b";
+fi
